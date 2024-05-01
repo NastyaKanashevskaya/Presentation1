95aa14dbfe862075e0d9e5b1cb0e03f315c5c39f
NastyaKanashevskaya <nastya.mozhzherina@gmail.com>
2024-05-01 12:50:18 +0300

    commit after reviewing and fixing

--- src/Task3/Task1.java
+++ src/Task3/Task1.java
@@ -1,30 +1,31 @@
 package Task3;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] a = new int[5];
-        for (int i = 0; i < a.length; i++) {
-            Random r = new Random();
-            a[i] = r.nextInt(5);
-            System.out.print(a[i] + " ");
+        int[] array = new int[10];
+        Random r = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = r.nextInt(100);
         }
-        System.out.println();
-        System.out.print("Enter the number: ");
-        int num = sc.nextInt();
-        boolean incl = false;
-        for (int i : a) {
-            if (num == i) {
-                incl = true;
-                System.out.println("Number is included in array");
+        Scanner scan = new Scanner(System.in);
+        System.out.println(Arrays.toString(array));
+        System.out.print("Enter the number of values in the array:  ");
+        int input = scan.nextInt();
+        boolean flag = false;
+        for (int j : array) {
+            if (j == input) {
+                flag = true;
                 break;
             }
-            System.out.println("Number is not included in array");
-            break;
+        }
+        if (flag) {
+            System.out.println("Number is in the array!");
+        } else {
+            System.out.println("Number is not found in the array!");
         }
     }
 }
-
